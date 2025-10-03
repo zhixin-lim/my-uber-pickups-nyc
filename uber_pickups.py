1580e601c43d320f8710ee44491c7ec30ed28802
zhixin-lim <zhixinnlimm@gmail.com>
2025-10-03 14:51:20 +0000

    changed to uber_pickups code

---
+++ uber_pickups.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# title
+st.title('Uber pickups in NYC')
+
+# loading data
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+# downloads some data, puts it in a Pandas dataframe, and converts the date column from text to datetime.
+# function accepts a single parameter (nrows) which specifies the number of rows that you want to load into the df
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text('Loading data...done!')
+
